37628ba48274c54acf762f79962f14fceade7b8e
deyvid102 <deyvidwellington@gmail.com>
2026-03-20 14:28:38 -0300

    correcao de nomeclatura api.js

--- frontend/src/components/pages/login/UserLogin.jsx
+++ frontend/src/components/pages/login/UserLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Mail, Lock, LogIn, Eye, EyeOff } from 'lucide-react';
 import { Link, useNavigate } from 'react-router-dom';
-import api from '../../../services/api';
+import api from "../../../services/api";
 // Importação da logo
 import logoImg from '../../../assets/logo.png';
 

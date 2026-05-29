6ffacf080debaa42df978d7f634183c81f01c81e
deyvid102 <deyvidwellington@gmail.com>
2026-05-29 12:29:28 -0300

    correcao de IPv4 para porta 465 email

--- backend/src/services/EmailService.js
+++ backend/src/services/EmailService.js
@@ -3,8 +3,9 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // false para usar STARTTLS na porta 587
+  port: 465,
+  secure: true, // true para SSL na porta 465
+  family: 4, // Força o uso de IPv4 para evitar erro ENETUNREACH (IPv6) no Render
   pool: true, // Mantém a conexão aberta para múltiplos envios
   auth: {
     user: process.env.EMAIL_USER, // Seu e-mail Gmail
